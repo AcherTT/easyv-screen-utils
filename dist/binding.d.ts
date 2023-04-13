b6f94bfe6d9da5bb7b5f79ba706b8ed3eeaec24a
AcherTT <937889516@qq.com>
2023-04-14 00:09:45 +0800

    feat: cmake-js

--- dist/binding.d.ts
+++
@@ -1,6 +0,0 @@
-declare class ScreenImportUtils {
-    private _addonInstance;
-    constructor(host: string, user: string, password: string, database: string, port: number);
-    importScreen(packetPath: string, callback: (err: Error, result: number) => void, time: number): string;
-}
-export = ScreenImportUtils;
